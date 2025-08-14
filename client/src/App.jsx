fea98e1125c4c7caea5c0c6a63bb0be61c07ea87
Raghul605 <raghulj.2001@gmail.com>
2025-08-14 21:02:47 +0530

    added notfound file on app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/Auth/ProtectedRoute';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
